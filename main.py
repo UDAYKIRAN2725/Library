14c2fdc54288ee65ea8cea2f82ca027bea9b8d6e
UDAYKIRAN2725 <116643279+UDAYKIRAN2725@users.noreply.github.com>
2022-12-28 14:59:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,22 +50,22 @@ Canvas1.pack(expand=True,fill=BOTH)
 headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
 headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
 
-headingLabel = Label(headingFrame1, text="Welcome to \n DataFlair Library", bg='black', fg='white', font=('Courier',15))
+headingLabel = Label(headingFrame1, text="F T B \n find the book", bg='black', fg='white', font=('Courier',18))
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
-btn1 = Button(root,text="Add Book Details",bg='black', fg='white', command=addBook)
+btn1 = Button(root,text="Add Book Details",bg='blue', fg='white', command=addBook)
 btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
     
-btn2 = Button(root,text="Delete Book",bg='black', fg='white', command=delete)
+btn2 = Button(root,text="Delete Book",bg='blue', fg='white', command=delete)
 btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
     
-btn3 = Button(root,text="View Book List",bg='black', fg='white', command=View)
+btn3 = Button(root,text="View Book List",bg='blue', fg='white', command=View)
 btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
     
-btn4 = Button(root,text="Issue Book to Student",bg='black', fg='white', command = issueBook)
+btn4 = Button(root,text="Issue Book to Student",bg='blue', fg='white', command = issueBook)
 btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
     
-btn5 = Button(root,text="Return Book",bg='black', fg='white', command = returnBook)
+btn5 = Button(root,text="Return Book",bg='blue', fg='white', command = returnBook)
 btn5.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
 virtualUI.Start (60)
 root.mainloop()
